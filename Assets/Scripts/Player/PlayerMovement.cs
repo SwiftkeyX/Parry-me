6c0496f3b0ba4f80a836dbff159f5a67ff6b1a35
SwiftkeyX <pisanucaraleadkul.pasit@gmail.com>
2026-03-17 15:27:23 +0700

    Feature: Add basic playermovement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    private InputController _inputController;
+    private CharacterController _characterController;
+
+    void Awake()
+    {
+        _inputController = GetComponent<InputController>();
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 moveDirection = new Vector3(1, 0, 1);
+        float moveSpeed = 3f;
+        _characterController.Move(moveDirection * moveSpeed);
+    }
 }
