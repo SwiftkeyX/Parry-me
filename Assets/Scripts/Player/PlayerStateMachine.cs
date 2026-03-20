0d41cccd01530b13d83fb7f3e5436d3f7e28f02c
SwiftkeyX <pisanucaraleadkul.pasit@gmail.com>
2026-03-20 16:15:57 +0700

    Adjust: del player's basic movement and move the logic to PlayerStateMachine for Reusability 2

--- Assets/Scripts/Player/PlayerStateMachine.cs
+++ Assets/Scripts/Player/PlayerStateMachine.cs
@@ -126,11 +126,14 @@ public class Idle : State
 
     protected override void CheckSwitchState()
     {
-        Debug.Log("MoveDirection from Idle: " + _bb.InputProcessor.MoveDirection);
-
-        if (_bb.InputProcessor.MoveDirection.sqrMagnitude > 0f)
+        if (_bb.InputProcessor.MoveDirection.sqrMagnitude > 0f && !_bb.InputProcessor.Run_input)
         {
             _bb.PlayerStateMachine.ChangeCurrentState(PlayerStateMachine.STATE.WALK);
+        }
+
+        else if (_bb.InputProcessor.MoveDirection.sqrMagnitude > 0f && _bb.InputProcessor.Run_input)
+        {
+            _bb.PlayerStateMachine.ChangeCurrentState(PlayerStateMachine.STATE.RUN);
         }
 
         base.CheckSwitchState();
@@ -170,6 +173,11 @@ public class Walk : State
             _bb.PlayerStateMachine.ChangeCurrentState(PlayerStateMachine.STATE.IDLE);
         }
 
+        else if (_bb.InputProcessor.MoveDirection.sqrMagnitude > 0f && _bb.InputProcessor.Run_input)
+        {
+            _bb.PlayerStateMachine.ChangeCurrentState(PlayerStateMachine.STATE.RUN);
+        }
+
         base.CheckSwitchState();
     }
 
@@ -193,10 +201,12 @@ public class Walk : State
 public class Run : State
 {
     private float _runSpeed;
+    private float _rotationSpeed;
 
-    public Run(StateMachineBlackBoard bb, float moveSpeed = 6f) : base(bb)
+    public Run(StateMachineBlackBoard bb, float moveSpeed = 6f, float rotationSpeed = 20f) : base(bb)
     {
         _runSpeed = moveSpeed;
+        _rotationSpeed = rotationSpeed;
     }
 
 
@@ -210,11 +220,39 @@ public class Run : State
     {
         base.OnUpdate();
 
+        _bb.CharacterController.Move(_bb.InputProcessor.MoveDirection * _runSpeed * Time.deltaTime);
+
+        RotateTowardMoveDirection();
     }
 
     protected override void CheckSwitchState()
     {
+        if (_bb.InputProcessor.MoveDirection.sqrMagnitude == 0f)
+        {
+            _bb.PlayerStateMachine.ChangeCurrentState(PlayerStateMachine.STATE.IDLE);
+        }
+
+        else if (_bb.InputProcessor.MoveDirection.sqrMagnitude > 0f && !_bb.InputProcessor.Run_input)
+        {
+            _bb.PlayerStateMachine.ChangeCurrentState(PlayerStateMachine.STATE.WALK);
+        }
 
         base.CheckSwitchState();
+    }
+
+    private void RotateTowardMoveDirection()
+    {
+        // shutup Unity's console log
+        if (_bb.InputProcessor.MoveDirection.sqrMagnitude < 0.001f) return;
+
+        // Get target rotation
+        Quaternion targetRotation = Quaternion.LookRotation(_bb.InputProcessor.MoveDirection);
+
+        // Smoothly rotate
+        _bb.transform.rotation = Quaternion.Slerp(
+            _bb.transform.rotation,
+            targetRotation,
+            _rotationSpeed * Time.deltaTime
+        );
     }
 }
